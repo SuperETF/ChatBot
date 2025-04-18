6b370040094764e992f4b9a1feb7ba3d0697d3ab
채정욱 <jeonguk95@gmail.com>
2025-04-18 23:05:15 +0900

    cnjnew

--- handlers/assignment/generateRoutinePreview.mjs
+++ handlers/assignment/generateRoutinePreview.mjs
@@ -1,6 +1,5 @@
-export function generateRoutine(goal = "") {
-    if (/하체/.test(goal)) return ["스쿼트 30개", "런지 20개", "점프스쿼트 10개"];
-    if (/상체/.test(goal)) return ["푸시업 20개", "딥스 15개", "플랭크 1분"];
-    return ["플랭크 1분", "마운틴클라이머 30초", "버피 10개"];
+export default function generateRoutine(goal = "") {
+    if (/하체/.test(goal)) return ["스쿼트 30개", "런지 20개"];
+    if (/상체/.test(goal)) return ["푸시업 20개", "딥스 15개"];
+    return ["플랭크 1분", "버피 10개"];
   }
-  
