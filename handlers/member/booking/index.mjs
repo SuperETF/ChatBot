999f4065b25a211e938324df3ca73d2197edc06c
채정욱 <jeonguk95@gmail.com>
2025-04-25 16:30:51 +0900

    cnjkwer

--- handlers/member/booking/index.mjs
+++ handlers/member/booking/index.mjs
@@ -1,8 +1,5 @@
 // ✅ handlers/booking/index.mjs
 import { reservePersonal, handleMultiTurnReserve, confirmReservation } from "./reservePersonal.mjs";
-import cancelPersonal from "./cancelPersonal.mjs";
-import confirmPendingTime from "./confirmPendingTime.mjs";
-import showMyReservations from "./showMyReservations.mjs";
 import { replyText } from "../../../utils/reply.mjs";
 
 export default async function booking(kakaoId, utterance, res, action) {
@@ -12,20 +9,6 @@ export default async function booking(kakaoId, utterance, res, action) {
 
     case "handleReserveMulti":
       return handleMultiTurnReserve(kakaoId, utterance, res);
-
-    case "confirmReservation":
-      // ex) confirmReservation 호출할 수도 있음
-      // confirmReservation(...) 
-      return res.json(replyText("직접 confirmReservation을 부르는 액션은 현재 없음."));
-    
-    case "cancelPersonal":
-      return cancelPersonal(kakaoId, utterance, res);
-
-    case "confirmPendingTime":
-      return confirmPendingTime(kakaoId, utterance, res);
-
-    case "showMyReservations":
-      return showMyReservations(kakaoId, utterance, res);
 
     default:
       return res.json(replyText(`예약 관련 기능(${action})을 찾지 못했습니다.`));
